10f7fc368bfec384bae2a340a4c09ec695e26f49
Togrul Hashimov <tosha.hashimov@googlemail.com>
2022-10-13 18:05:03 +0100

    trying to find a way to make the grid with less code

--- src/components/chessBoard.tsx
+++ src/components/chessBoard.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ChessPieces from './pieces.json';
 
 interface ChessBoardProps {
     
@@ -14,51 +13,42 @@ const ChessBoard: React.FC<ChessBoardProps> = () => {
     }   
 
     for (let i = 0; i < Math.pow(cells, 2); i++) {
-        let src: string = '';
+        let whichClass = 'cell';
+        let num = 0;
+       
         
-        switch (true) {
-            case (i === 0 || i === 7):
-                src = ChessPieces.rookBlack;
-                break;
-            case(i === 1 || i === 6):
-                src = ChessPieces.knightBlack;
-                break;
-            case(i === 2 || i === 5):
-                src = ChessPieces.bishopBlack;
-                break;
-            case(i === 3):
-                src = ChessPieces.queenBlack;
-                break;
-            case(i === 4):
-                src = ChessPieces.kingBlack;
-                break;
-            case(i >= 8 && i <= 15):
-                src = ChessPieces.pawnBlack;
-                break;
-            case(i >= 48 && i <= 55):
-                src = ChessPieces.pawnWhite;
-                break;
-            case(i === 56 || i === 63):
-                src = ChessPieces.rookWhite;
-                break;
-            case(i === 57 || i === 62):
-                src = ChessPieces.knightWhite;
-                break;
-            case(i === 58 || i === 61):
-                src = ChessPieces.bishopWhite;
-                break;
-            case(i === 59):
-                src = ChessPieces.queenWhite;
-                break;
-            case(i === 60):
-                src = ChessPieces.kingWhite;
-                break;
+        if (i < 8) {
+            if ((i | 1) > i) {
+                    whichClass = `${whichClass} white`
+            } else {
+                whichClass = `${whichClass} black`
+            }
+        } else if (i >= 8 && i <= 15) {
+            if ((i | 1) > i) {
+                whichClass = `${whichClass} black`
+            } else {
+                whichClass = `${whichClass} white`
+            }
+        } else if (i >= 16 && i <= 23) {
+            if ((i | 1) > i) {
+                whichClass = `${whichClass} white`
+            } else {
+                whichClass = `${whichClass} black`
+            }
+        } else if (i >= 24 && i <= 31) {
+            if ((i | 1) > i) {
+                whichClass = `${whichClass} black`
+            } else {
+                whichClass = `${whichClass} white`
+            }
         }
 
-        const cell: JSX.Element = <div className='cell' key={i} onClick={() => {handleClick(i)}}>
-            <img src={src} alt=''></img>
+
+        const cell: JSX.Element = <div className={whichClass}  key={i} onClick={() => {handleClick(i)}}>
+            <img src='' alt=''></img>
         </div>
         boardCells.push(cell);
+        num ++;
     }
 
 
