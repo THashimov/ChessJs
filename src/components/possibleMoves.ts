b6716289bc249bbd2c281c86a28df40db921bf69
Togrul Hashimov <tosha.hashimov@googlemail.com>
2022-10-18 12:36:14 +0100

    possible moves working for every piece

--- src/components/possibleMoves.ts
+++ src/components/possibleMoves.ts
@@ -9,21 +9,8 @@ import Rook from "../classes/Rook";
 const possibleMoves = (piece: Bishop | King | Knight | Pawn | Queen | Rook, chessBoard: ChessBoard) => {
     // Clear the board
     [...document.querySelectorAll('.possibleMove')].map(e => e.classList.remove('possibleMove'));
+    [...document.querySelectorAll('.canTake')].map(e => e.classList.remove('canTake'));
 
-    // Bishop has 4 directions of movement
-    // Once we hit a piece, we could break from this and check the next direction
-    // How do we represent this?
-
-
-    // Testing a deeper array
-    // Possible moves is an array with 4 elements
-    // Those 4 elements have 6 elements inside them
-    // Those 6 elements hold a coord
-
-
-    // Bishop is at [4, 7]
-
-    // 
 
     for (let i = 0; i < piece.possibleMoves.length; i++) {
         for (let j = 0; j < piece.possibleMoves[i].length; j++) {
@@ -31,67 +18,34 @@ const possibleMoves = (piece: Bishop | King | Knight | Pawn | Queen | Rook, ches
             let col = piece.possibleMoves[i][j][1];
             let row = piece.possibleMoves[i][j][0];
 
-            // let cell = document.querySelector(`[data-value="${row},${col}"]`)
-            // cell?.classList.add('yellow')
-
-            console.log('col :', col);
-            console.log('row :', row);
-
             if (row < 0 || row > 7) {
                 break;
             } else if (col < 0 || col > 7) {
                 break;
             }
 
-            
-
-
             if (chessBoard.cells[row][col] === null) {
                 let cell = document.querySelector(`[data-value="${row},${col}"]`)
                 cell?.classList.add('possibleMove')
             } else if (chessBoard.cells[row][col].color === 'black'){
-                let cell = document.querySelector(`[data-value="${row},${col}"]`)
-                cell?.classList.add('yellow')
+                if (piece.type === 'pawn') {
+                    let offset: number = 1;
+                    for (let i = 0; i < 2; i++) {
+                        let cell = document.querySelector(`[data-value="${row},${col - offset}"]`)
+                        cell?.classList.add('canTake');
+                        offset *= (-1);
+                    }
+                    break;
+                } else {
+                    let cell = document.querySelector(`[data-value="${row},${col}"]`)
+                    cell?.classList.add('canTake');
+                    break;
+                }
+            } else {
                 break;
-            }   
-
+            }
         }
-
-
-
-
-
-
-
-
-
-        // let col = piece.possibleMoves[i][1] % 8;
-        // let row = piece.possibleMoves[i][0] % 8;
-
-        // if (row < 0) {
-        //     continue;
-        // };        
-        // if (col < 0) {
-        //     continue;
-        // };
-
-     
-        
-
-        // // if (row >= 0) {
-        //     // let cell = document.querySelector(`[data-value="${row},${col}"]`)
-        //     // cell?.classList.add('possibleMove')
-        // // }
-        // // console.log(col)
-
-        // if (chessBoard.cells[row][col] === null) {
-        //     let cell = document.querySelector(`[data-value="${row},${col}"]`)
-        //     cell?.classList.add('possibleMove')
-        // } else {
-        //     continue;
-        // }
     }
-
 }
 
 export default possibleMoves;
